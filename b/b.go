76b78b5b384b018005d31a85246fb340448cae26
Archana Shinde <archana.m.shinde@intel.com>
2019-07-25 17:19:41 -0700

    Add package b

---
+++ b/b.go
@@ -0,0 +1,3 @@
+package b // import "github.com/amshinde/go-modules-by-example-submodules/b"
+
+const Name = "Gopher"
---
+++ b/go.mod
@@ -0,0 +1,3 @@
+module github.com/amshinde/go-modules-by-example-submodules/b
+
+go 1.12
